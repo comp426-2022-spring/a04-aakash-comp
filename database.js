6260276c8ae28f812d06f49c00c974bfbb3f8967
root <root@Mainframe.localdomain>
2022-04-27 01:01:45 -0400

    column add

--- database.js
+++ database.js
@@ -11,7 +11,17 @@ if (row === undefined) {
     console.log('Log database missing. Create log database')
     
     const sqlInit = `
-        CREATE TABLE IF NOT EXISTS access ( id INTEGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR)`
+        CREATE TABLE IF NOT EXISTS access ( id INTEGER PRIMARY KEY,
+            remoteaddr VARCHAR,
+            remoteuser VARCHAR,
+            time VARCHAR,
+            method VARCHAR,
+            url VARCHAR,
+            protocol VARCHAR,
+            httpversion NUMERIC,
+            status INTEGER,
+            referer VARCHAR,
+            useragent VARCHAR)`
     
     logdb.exec(sqlInit)
 }
